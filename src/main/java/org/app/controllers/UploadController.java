cb078c8e257c700b12d4fd42179ab776a000e0bb
altinkaya <124771353+altinkaya@users.noreply.github.com>
2024-06-01 16:13:16 +0200

    sad
    asd

--- src/main/java/org/app/controllers/UploadController.java
+++ src/main/java/org/app/controllers/UploadController.java
@@ -1,8 +1,11 @@
 package org.app.controllers;
 
+import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import io.javalin.http.UploadedFile;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,6 +22,21 @@ public class UploadController {
             ctx.status(200).json(Map.of("message", "File uploaded successfully", "fileName", fileName));
         } else {
             ctx.status(400).result("No file uploaded");
+        }
+    };
+
+    public static Handler sendFile = ctx -> {
+        String fileName = ctx.pathParam("filename");
+        Path filePath = Paths.get("/api/images", fileName);
+        if (Files.exists(filePath)) {
+            try {
+                FileInputStream fis = new FileInputStream(filePath.toFile());
+                ctx.result(fis);
+            } catch (IOException e) {
+                ctx.status(500).result("Error reading file");
+            }
+        } else {
+            ctx.status(404).result("File not found");
         }
     };
 }
